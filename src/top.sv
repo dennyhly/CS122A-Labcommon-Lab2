321a4337f9ae1256e28e1cc57a4bb4840c4b7c06
dennyhly <dennyly626@gmail.com>
2026-04-07 20:11:48 -0700

    7 seg simulation

--- src/top.sv
+++ src/top.sv
@@ -1,8 +1,36 @@
 module top (
     /** Input Ports */
+    input wire [3:0] bcd,
     /** Output Ports */
+    output logic [6:0] seg7
 );
 
 /** Logic */
+wire a = bcd[3];
+wire b = bcd[2];
+wire c = bcd[1];
+wire d = bcd[0];
+/** would have been a lot faster if i just just case statements */
+always @(*) begin
+    case (bcd)
+        4'h0: seg7 = 7'b1111110; 
+        4'h1: seg7 = 7'b0110000;
+        4'h2: seg7 = 7'b1101101;
+        4'h3: seg7 = 7'b1111001;
+        4'h4: seg7 = 7'b0110011;
+        4'h5: seg7 = 7'b1011011;
+        4'h6: seg7 = 7'b1011111;
+        4'h7: seg7 = 7'b1110000;
+        4'h8: seg7 = 7'b1111111;
+        4'h9: seg7 = 7'b1111011;
+        4'hA: seg7 = 7'b1110111;
+        4'hB: seg7 = 7'b0011111;
+        4'hC: seg7 = 7'b1001110;
+        4'hD: seg7 = 7'b0111101;
+        4'hE: seg7 = 7'b1001111;
+        4'hF: seg7 = 7'b1000111;
+        default: seg7 = 7'b0111111; 
+    endcase
+end
 
 endmodule
--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -3,14 +3,17 @@
 
 module top_tb;
 logic clk;
+logic [3:0] bcd;
+logic [6:0] seg7;
 
-top 
+top top_uut
 (
-    .clk(clk)
     /** add additional signals */
+    .bcd(bcd),
+    .seg7(seg7)
 );
 
-localparam CLK_PERIOD = /** clk period */;
+localparam CLK_PERIOD = 5;
 always #(CLK_PERIOD/2) clk=~clk;
 
 initial begin
@@ -22,6 +25,40 @@ initial begin
     clk<=1'b1;
     #(CLK_PERIOD*3);
     // testbench logic goes below
+    bcd <= 4'd0;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd1;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd2;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd3;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd4;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd5;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd6;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd7;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd8;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd9;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd10;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd11;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd12;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd13;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd14;
+    #(CLK_PERIOD*3);
+    bcd <= 4'd15;
+    #(CLK_PERIOD*3);
+
+
     $finish;
 end
 
